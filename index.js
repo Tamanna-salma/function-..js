e8cbf417f6aca33c1e37106cfa21a4aafb8647ea
Tamanna-salma <tamannaummey26@gmail.com>
2025-01-21 12:06:14 +0600

    unary operator added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function myskill(){
+/*function myskill(){
 
     document.write('HTML </br>');
     document.write('CSS <br>');
@@ -23,7 +23,7 @@ document.write('----------EnD---------<br>');
 
 function cost(){
     
-    let rent=100;
+    let rent=1000;
     let salary=500;
     document.write( `the sum of total cost =${rent+salary}<br>`);
     document.write( `the substruct of total cost =${rent-salary}<br>`);
@@ -32,4 +32,48 @@ function cost(){
 }
 
 
-cost();
+cost();*/
+
+                    // unary operator
+
+let number1 = parseInt(prompt());
+let number2 = parseInt(prompt());
+
+let result =number1+ number2;
+document.write(result);
+
+// short hand**
+
+let Number1 = +prompt();
+let Number2 = +prompt();
+
+let Result = number1+ number2;
+document.write(Result);
+
+let number3 = prompt();
+let number4 = prompt();
+
+let sum = +number1+ +number2;
+document.write(sum);
+
+//   ***tarnary operator*****
+
+let myage =17;
+if(myage>=18){
+    console.log('you can vote');
+}
+else{
+    console.log('you are not eligible for submitting vote');
+
+}
+
+// short hand***
+
+// 1..condition
+// 2.. ?
+// 3.. :
+//   condition true value (if) false value (else)
+let Myage =17;
+Myage>=18 ? console.log('you can vote') : console.log('you are not eligible for submitting vote');
+
+
