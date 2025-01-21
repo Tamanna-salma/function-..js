4d24485aae2b6639fbf412622b845e72692cd586
Tamanna-salma <tamannaummey26@gmail.com>
2025-01-21 10:27:33 +0600

    html file added

---
+++ index.js
@@ -0,0 +1,35 @@
+function myskill(){
+
+    document.write('HTML </br>');
+    document.write('CSS <br>');
+    document.write('Tailwind <br>');
+    document.write('Hey! Good news ! I learned javascript<br>');
+    document.write('----------EnD---------<br>');
+}
+myskill();
+myskill();
+myskill();
+myskill();
+myskill();
+myskill();
+
+
+document.write('HTML </br>');
+document.write('CSS <br>');
+document.write('Tailwind <br>');
+document.write('----------EnD---------<br>');
+
+
+
+function cost(){
+    
+    let rent=100;
+    let salary=500;
+    document.write( `the sum of total cost =${rent+salary}<br>`);
+    document.write( `the substruct of total cost =${rent-salary}<br>`);
+    document.write( `the multiplication of total cost =${rent*salary}<br>`);
+    document.write( `the dividend of total cost =${rent/salary}<br>`);
+}
+
+
+cost();
